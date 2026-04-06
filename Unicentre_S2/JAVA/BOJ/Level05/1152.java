5f6235b2197e155c3727185e50eca2a096b25c7c
Yunyi Han <yunyiverse0302@gmail.com>
2026-04-06 15:28:45 +0900

    단어의 개수

---
+++ Unicentre_S2/JAVA/BOJ/Level05/1152.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                if (i == 0 || s.charAt(i - 1) == ' ') {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+        sc.close();
+    }
+}
