824dd1f1f60491b3878f7d4901bcc9d1e5eb06bc
Yunyi Han <yunyiverse0302@gmail.com>
2026-04-13 15:04:27 +0900

    Count Occurrences

---
+++ Unicentre_S2/JAVA/quiz/Q4/q3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int maxCount = 0;
+
+		for (int i = 0; i < n; i++) {
+			int count = 0;
+			for (int j = 0; j < n; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+				}
+			}
+			if (count > maxCount) {
+				maxCount = count;
+			}
+		}
+
+		System.out.println(maxCount);
+	}
+}
